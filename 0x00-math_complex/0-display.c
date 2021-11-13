26e95c3cb9c1a5ace9c155e10ba113c3159ca9e4
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-13 17:01:56 +0100

    hot fix

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -6,6 +6,15 @@
  */
 void display_complex_number(complex c)
 {
+if (c.re == 0)
+{
+if (c.im < 0)
+printf("- %.9gi\n", c.im * (-1));
+else if (c.im == 0)
+printf("0\n");
+else
+printf("%.9g\n", c.im);
+}
 if (c.re < 0 && c.im < 0)
 printf("%.9g - %.9gi", c.re, c.im * (-1));
 else if (c.re < 0 || c.im < 0)

e379ad8bd1409f199fe56b1f23bfc4d9096e0a24
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-13 16:54:58 +0100

    displaying complex numbers

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -6,5 +6,16 @@
  */
 void display_complex_number(complex c)
 {
-  
+if (c.re < 0 && c.im < 0)
+printf("%.9g - %.9gi", c.re, c.im * (-1));
+else if (c.re < 0 || c.im < 0)
+{
+if (c.re < 0)
+printf("%.9g + %.9gi", c.re, c.im);
+else
+printf("%.9g - %.9gi", c.re, c.im * (-1));
+}
+else
+printf(" %.9g + %.9gi", c.re, c.im);
+printf("\n");
 }

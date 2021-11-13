c5b3791916acbd1de4c4b7cf55ddaffd96f8c7b2
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-11-13 15:59:28 +0100

    Header init

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,10 @@
+#include "complex.h"
+/**
+ * display_complex_number - displaying a complex number
+ * @c: complex
+ * Return - void
+ */
+void display_complex_number(complex c)
+{
+  
+}
---
+++ 0x00-math_complex/complex.h
@@ -0,0 +1,19 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+#include <stdarg.h>
+
+/**
+ * struct nb_complex - complex type
+ * @re: double
+ * @im: double
+ */
+typedef struct nb_complex
+{
+double re;
+double im;
+} complex;
+
+void display_complex_number(complex c);
+
+#endif
